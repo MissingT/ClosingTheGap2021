122150e8e20e947bc2f563dc72f4b14ac420fcbf
MissingT <79103459+MissingT@users.noreply.github.com>
2021-08-08 14:23:56 -0400

    Fixed JS issues Again

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 $(document).ready(function () {
-
-function myFunction() {
-    var x = document.getElementById("menuPullout");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-      $("body").css("overflow", "auto")
-    } else {
-      x.style.display = "block";
-      $("body").css("overflow", "hidden")
-    }
-  }
-
-
     $(".benefitButton").click(function(event){
       $(event.target).next().slideToggle();
     });
-
 });
+
+function myFunction() {
+  var x = document.getElementById("menuPullout");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    $("body").css("overflow", "auto")
+  } else {
+    x.style.display = "block";
+    $("body").css("overflow", "hidden")
+  }
+}
